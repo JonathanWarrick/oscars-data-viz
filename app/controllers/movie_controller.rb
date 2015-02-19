b9dc8e9628d54c602149eaed990c646f81e25d6a
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-02-18 22:15:23 -0500

    Add endpoints for years.

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -2,4 +2,9 @@ class MovieController < ApplicationController
 	def show
 		@movies = Movie.all
 	end
+
+	def show_year
+		year = params[:year]
+		render json: Movie.where(:year => year).to_a
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'movie#show'
 
+  get 'movies' => 'movie#show_year'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
