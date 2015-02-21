c880cabf4120a7c843a684bb60484797ceb3f86a
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-02-21 14:22:24 -0500

    Adds CSS for C3. Averages charts render completely now.

--- app/assets/javascripts/d3_code.js
+++ app/assets/javascripts/d3_code.js
@@ -32,7 +32,18 @@ function fetchAverages() {
     url: 'movie/get_averages',
     dataType: 'json',
     success: function (data) {
-      console.log(data)
+      console.log(data);
+      criticsScores = ["Critics"];
+      audienceScores = ["Audience"];
+      movieYears = ['years'];
+
+      for(var i = 1934; i <= 2014; i++) {
+        movieYears.push('' + i + '-01-01');
+        criticsScores.push(data.critics_scores[i.toString()]);
+        audienceScores.push(data.audience_scores[i.toString()]);
+      }
+
+      produceAveragesChart(criticsScores, audienceScores, movieYears);
     },
     error: function (result) {
       error();
@@ -54,7 +65,6 @@ function fetchWinners() {
     }
   });
 }  
- 
 
 function produceByYearChart(cScores, aScores, titles) {
   var chart = c3.generate({
@@ -75,6 +85,50 @@ function produceByYearChart(cScores, aScores, titles) {
     axis: {
       x: {
         type: 'category' // this needed to load string x value
+      }
+    }
+  });
+}
+
+function produceAveragesChart(cScores, aScores, years) {
+  var chart = c3.generate({
+    bindto: '#averagesChart',
+    // remove tooltip for now
+    interaction: {
+      enabled: false
+    },
+    data: {
+      x: 'years',
+      columns: [years, cScores, aScores]
+    }, 
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%Y'
+        }
+      }
+    }
+  });
+}
+
+function produceWinnersChart(cScores, aScores, years) {
+  var chart = c3.generate({
+    bindto: '#averagesChart',
+    // remove tooltip for now
+    interaction: {
+      enabled: false
+    },
+    data: {
+      x: 'years',
+      columns: [years, cScores, aScores]
+    }, 
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%Y'
+        }
       }
     }
   });
